7b9250b5840457eb76fd566a6ae67a43fe7bf36a
minnos <elvanarvis@hotmail.com>
2022-08-13 14:39:12 +0300

    -Splash Screen -App Logo

--- app/src/main/java/com/simtrayclub/avatarkorracharacters/SplashActivity.kt
+++ app/src/main/java/com/simtrayclub/avatarkorracharacters/SplashActivity.kt
@@ -1,11 +1,28 @@
 package com.simtrayclub.avatarkorracharacters
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.simtrayclub.avatarkorracharacters.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+
+            splashImage.alpha = 0f
+            splashImage.animate().setDuration(2000).alpha(1f).withEndAction{
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                finish()
+            }
+        }
     }
 }
