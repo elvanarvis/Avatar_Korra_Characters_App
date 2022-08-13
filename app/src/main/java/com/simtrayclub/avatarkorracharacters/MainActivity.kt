a38bd6f16679025b9579fd08e04eeef433e62c0d
minnos <elvanarvis@hotmail.com>
2022-08-14 00:07:33 +0300

    -Update logo -Toolbar -Fragment -Multi screen files

--- app/src/main/java/com/simtrayclub/avatarkorracharacters/MainActivity.kt
+++ app/src/main/java/com/simtrayclub/avatarkorracharacters/MainActivity.kt
@@ -2,10 +2,27 @@ package com.simtrayclub.avatarkorracharacters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.simtrayclub.avatarkorracharacters.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.toolbar.title= "  Characters"
+        binding.toolbar.setLogo(R.drawable.wind_logo_36)
+
+        setSupportActionBar(binding.toolbar)
+
+        //val navHostFragment =
+          //  supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+
     }
 }
